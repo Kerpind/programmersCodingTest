72682e22faf18b6764ee2c5090076d3bb38f1c0f
Kerpind <sjwan794@gmail.com>
2023-12-18 10:09:03 +0900

    commit

--- src/Test2.java
+++ src/Test2.java
@@ -1,9 +1,40 @@
+import java.util.*;
+
 public class Test2 {
 
     public static void main(String[] args) {
-        String a = "무궁화. 삼천리. 화려강산. 대한사람. 대한으로. 길이. 보전하세 ";
+        String a = "-2-";
 
-        a = a.replace(".", "/");
-        System.out.println(a);
+        List<String> a1 = new ArrayList<>();
+        List<String> a2 = new ArrayList<>();
+        List<String> a3 = new ArrayList<>();
+
+        String[] arr = new String[3];
+        arr = a.split("-");
+
+        System.out.println("Arrays.toString(arr) => " + Arrays.toString(arr));
+        System.out.println("arr.length => " + arr.length);
+        System.out.println("arr[0].length() => " + arr[0].length());
+        System.out.println("arr[1].length() => " + arr[1].length());
+        System.out.println("arr[2].length() => " + arr[2].length());
+
+        if (arr.length == 0) {
+            System.out.println("111111111");
+        } else {
+            System.out.println("222222222");
+        }
+
+        System.out.println(arr[0].length());
+        System.out.println(arr[1].length());
+        System.out.println(arr[2].length());
+
+        a1 = Arrays.asList(arr[0].split("/"));
+        a2 = Arrays.asList(arr[1].split("/"));
+        a3 = Arrays.asList(arr[2].split("/"));
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+
     }
 }
---
+++ src/introduction/Test3.java
@@ -0,0 +1,15 @@
+package introduction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        String a = "banner\\/20231213\\/41ac2b928dd0ad24acf6a1619829ee8b.jpg";
+
+        System.out.println(a.replaceAll("\\\\", ""));
+
+    }
+}
