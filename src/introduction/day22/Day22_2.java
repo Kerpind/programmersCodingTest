38b8097b42da3afa6755deb3c4c2f42dbb2c6660
Kerpind <sjwan794@gmail.com>
2024-01-19 11:10:48 +0900

    test commit

---
+++ src/introduction/day22/Day22_2.java
@@ -0,0 +1,60 @@
+package introduction.day22;
+
+public class Day22_2 {
+
+    // 저주의 숫자 3
+    public static void main(String[] args) {
+
+        int n = 9;
+
+        System.out.println("solution => " + solution(n));
+
+    }
+
+//    1	    1   (0)
+//    2	    2   (0)
+//    3	    4   (1)
+//    4	    5   (1)
+//    5	    7   (2)
+//    6	    8   (2)
+//    7	    10  (3)
+//    8	    11  (3)
+//    9	    14  (5)
+//    10	16  (6)
+
+    public static int solution(int n) {
+
+        int answer = 0;
+        int cnt = 1;
+        boolean check = false;
+        String numstr = "";
+
+        while(true){
+
+            check = false;
+
+            if(answer % 3 == 0){
+                answer++;
+                continue;
+            }
+
+            numstr = String.valueOf(answer);
+
+            if(numstr.contains("3")){
+                answer++;
+                check = true;
+            }
+
+            if(check) continue;
+
+            if(cnt == n)break;
+
+            answer++;
+            cnt++;
+
+        }
+        return answer;
+
+    }
+
+}
