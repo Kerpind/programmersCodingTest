64afd51de02a67b4e73114b8c05bbb00aedc3ff4
Kerpind <sjwan794@gmail.com>
2024-01-19 11:05:51 +0900

    commit

--- src/Test4.java
+++ src/Test4.java
@@ -3,7 +3,17 @@ public class Test4 {
     public static void main(String[] args) {
 
 
-        System.out.println(9 % 3);
+        System.out.println(0 % 3);
+
+        int a = 0;
+
+        while (true) {
+            if(a != 5){
+                a++;
+                System.out.println("?????");
+                continue;
+            }
+        }
 
     }
 }
--- src/introduction/day22/Day22_1.java
+++ src/introduction/day22/Day22_1.java
@@ -25,28 +25,30 @@ public class Day22_1 {
     public static int solution(int n) {
 
         int answer = 0;
-        int cnt=1;
-        boolean check=false;
-        String numstr ="";
-        while(true){
-            check=false;
+        int cnt = 1;
+        boolean check = false;
+        String numstr = "";
 
-            if(answer%3==0){
+        while(true){
+
+            check = false;
+
+            if(answer % 3 == 0){
                 answer++;
                 continue;
             }
-            numstr=String.valueOf(answer);
-            //else{
+
+            numstr = String.valueOf(answer);
+
             if(numstr.contains("3")){
                 answer++;
-                check=true;
-                //break;
+                check = true;
             }
-            //}
 
             if(check) continue;
 
-            if(cnt==n)break;
+            if(cnt == n)break;
+
             answer++;
             cnt++;
 
