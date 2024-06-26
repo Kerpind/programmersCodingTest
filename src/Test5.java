025bc95dc01a2e6e612d7042c1b36c7c43c1e6be
Kerpind <sjwan794@gmail.com>
2024-06-26 09:38:58 +0900

    test5 commit

--- src/Test5.java
+++ src/Test5.java
@@ -2,25 +2,33 @@ public class Test5 {
 
     public static void main(String[] args) {
 
-        // 2
+        // tickets: 티켓별 금액(A: 1원, B: 2원, ..)
+        // roll: shop에 진열된 상품을 새로고침 할 때 필요한 금액
+        // shop: 진열된 상품(배열의 개수는 n개 1번째에서 2번째로 넘어갈 때 마다 roll의 금액만큼 money가 차감 됨)
+        // money: 총 내가 받은 금액
+
+        // 같은 알파벳의 티켓이 3장 모이면 황금 티켓으로 교환이 가능합니다.
+        // 가장 황금 티켓을 금액에 맞게 많이 구할 수 있는 갯수를 구하면 됩니다.
+
+        // result: 2
         String[] tickets = new String[]{"A 1", "B 2", "C 5", "D 3"}; // 1~10000
         int roll = 10; // 1000
         String[][] shop = new String[][]{{"B", "C", "B", "C"}, {"A", "A", "A", "B"}, {"D", "B", "C", "D"}};
         int money = 30; // 100000000
 
-        // 4
+        // result: 4
         // String[] tickets = new String[]{"A 1", "B 2", "C 5", "D 3"};
         // int roll = 10;
         // String[][] shop = new String[][]{{"B", "C", "B", "C"}, {"A", "A", "A", "B"}, {"D", "B", "C", "D"}};
         // int money = 100;
 
-        // 2
+        // result: 2
         // String[] tickets = new String[]{"A 2", "B 1"};
         // int roll = 1;
         // String[][] shop = new String[][]{{"A", "A", "A"}, {"A", "B", "B"}, {"A", "B", "B"}, {"A", "B", "B"}};
         // int money = 9;
 
-        // 2
+        // result: 2
         // String[] tickets = new String[]{"A 2", "B 1"};
         // int roll = 5;
         // String[][] shop = new String[][]{{"A", "A", "A"}, {"A", "B", "B"}, {"A", "B", "B"}, {"A", "B", "B"}};
